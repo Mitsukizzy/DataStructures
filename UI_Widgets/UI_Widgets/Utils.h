36cb8f19b41865c582bfce8f3c8a7820a5c703ad
Izzy Benavente <ibenaven@usc.edu>
2017-06-30 20:51:54 -0700

    Added more utils to vectors classes, some work on waypoint

--- UI_Widgets/UI_Widgets/Utils.h
+++ UI_Widgets/UI_Widgets/Utils.h
@@ -11,11 +11,20 @@ public:
 	float y;
 	float z;
 
+	// Default constructor
 	Vector3():x(0.0f), y(0.0f), z(0.0f)
 	{}
 
-	Vector3( float inX, float inY, float inZ ):x(inX), y(inY), z(inZ)
+	Vector3( float newX, float newY, float newZ ):x(newX), y(newY), z(newZ)
 	{}
+
+	// Set all, useful after instantiation
+	void Set( float newX, float newY, float newZ )
+	{
+		x = newX;
+		y = newY;
+		z = newZ;
+	}
 
 	float LengthSq()
 	{
@@ -42,18 +51,35 @@ public:
 		return vNorm;
 	}
 
-	float Dot( const Vector3 a, const Vector3 b )
+	float Dot( const Vector3 v )
 	{
-		return ( a.x * b.x + a.y * b.y + a.z * b.z );
+		return ( x * v.x + y * v.y + z * v.z );
 	}
 
-	Vector3 Cross( const Vector3 a, const Vector3 b )
+	Vector3 Cross( const Vector3 v )
 	{
 		Vector3 product;
-		product.x = a.y * b.z - a.z * b.y;
-		product.y = a.z * b.x - a.x * b.z;
-		product.z = a.x * b.y - a.y * b.x;
+		product.x = y * v.z - z * v.y;
+		product.y = z * v.x - x * v.z;
+		product.z = x * v.y - y * v.x;
 		return product;
+	}
+
+	// OPERATOR OVERRIDES
+	// friend keyword allows non-member functions to access private and protected memobers of a class
+	friend Vector3 operator+( const Vector3 a, const Vector3 b )
+	{
+		return Vector3( a.x + b.x, a.y + b.y, a.z + b.z );
+	}
+
+	friend Vector3 operator-( const Vector3 a, const Vector3 b )
+	{
+		return Vector3( a.x - b.x, a.y - b.y, a.z - b.z );
+	}
+
+	friend Vector3 operator*( const Vector3 a, const Vector3 b )
+	{
+		return Vector3( a.x * b.x, a.y * b.y, a.z * b.z );
 	}
 
 }; // End Vector3 class
@@ -64,11 +90,44 @@ public:
 	float x;
 	float y;
 
+	// Default constructor
 	Vector2():x(0.0f), y(0.0f)
 	{}
 
-	Vector2( float inX, float inY ):x(inX), y(inY)
+	Vector2( float newX, float newY ):x(newX), y(newY)
 	{}
+
+	// Set all, useful after instantiation
+	void Set( float newX, float newY )
+	{
+		x = newX;
+		y = newY;
+	}
+
+	float LengthSq()
+	{
+		return ( x * x + y * y );
+	}
+
+	float Length()
+	{
+		return sqrtf( LengthSq() );
+	}
+
+	void Normalize()
+	{
+		float len = Length();
+		x /= len;
+		y /= len;
+	}
+
+	Vector3 Normalize( const Vector3 v )
+	{
+		Vector3 vNorm = v;
+		vNorm.Normalize();
+		return vNorm;
+	}
+
 
 	float Dot( const Vector2 a, const Vector2 b )
 	{
--- UI_Widgets/UI_Widgets/Waypoint.cpp
+++ UI_Widgets/UI_Widgets/Waypoint.cpp
@@ -5,15 +5,37 @@ class Waypoint
 {
 public:
 	Vector3 arrowFacing;
-	Vector3 arrowDest;
 	Vector2 arrowScreenPos;
+
+	Vector3 playerPos;
+	Vector3 playerDest;
 
 	void Update( float deltaTime )
 	{
 		// New facing is dest - currentPos, then normalize
+		Vector3 newFacing = playerDest - playerPos;
+		newFacing.Normalize();
+
 		// Angle theta is facing dot dest
+		float theta = arrowFacing.Dot( newFacing );
+
 		// Axis is facing cross dest
+		Vector3 axis = arrowFacing.Cross( newFacing );
+
 		// If not parallel, make quaternion but normalize axis first
+		// TODO: Create quaternion impl
+		// Quaternion q;
+
+		if ( axis.Length() < 0.001f )
+		{
+			// q = quat identity
+		}
+		else
+		{
+			axis.Normalize();
+			// create q from angle and axis
+		}
+
 	}
 
 };
